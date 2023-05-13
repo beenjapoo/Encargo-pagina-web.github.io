3ebba73385a56918bbd30def97774dd8e75541a0
beenjapoo <ben.arayas@duocuc.cl>
2023-05-13 14:20:23 -0400

    cambios de cabezera

---
+++ js/ajax2.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",(e)=>{
+
+    const url = "../navbar2.html"
+    const httpx = new XMLHttpRequest();
+
+    httpx.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            document.querySelector(".minav").innerHTML=this.responseText;
+        }
+    }
+
+    httpx.open("get",url)
+    httpx.send()
+
+})
